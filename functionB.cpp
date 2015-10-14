a0fad0668ec7fae47489118ec362d2a39e609386
Emily Garcia <emily1597@gmail.com>
2015-10-13 17:21:54 -0700

    Created fourth function set

--- functionB.cpp
+++ functionB.cpp
@@ -10,11 +10,13 @@ void  feetToInches(double& val1 , double& val2);
 double computeRectangle(double valA, double valB);
 void computerArea (double valA, double valB, double& valC);
 void computerArea (double valA, double valB, double& valArea, double& valPerimeter);
+void  stats(double valA, double valB, double valC, double valD, double& valE, double& valF);
+void calcAreaPerimeter(double radius, double& area, double& perimeter);
 
 int main ()
 {
    
-   double num1, num2, num3, num4;// Declare variable
+   double num1, num2, num3, num4, num5, num6;// Declare variable
    
 /*   //Test Case: Set #1
    num1 = 2.0;
@@ -27,7 +29,7 @@ int main ()
    num2 = 5.0;
    cout <<  computeRectangle(num1, num2) << endl;
    computerArea(num1, num2, num3);
-   cout << num3 << endl;*/
+   cout << num3 << endl;
    
    //Test Case: Set #3
    num1 = 2;
@@ -35,6 +37,21 @@ int main ()
    computerArea(num1, num2, num3, num4);
    cout << num3 << endl;
    cout << num4 << endl;
+   
+   //Test Case: Set #4
+   num1 = 1;
+   num2 = 2;
+   num3 = 3;
+   num4 = 4;
+   stats(num1, num2, num3, num4, num5, num6);
+   cout << num5 << endl;
+   cout << num6 << endl;*/
+   
+   //Test Case: Set #5
+   num1 = 5;
+   calcAreaPerimeter(num1, num2, num3);
+   cout << num2 << endl;
+   cout << num3 << endl;
    
    return 0;
 }
@@ -55,10 +72,23 @@ void computerArea(double valA, double valB, double& valC)
 {
    valC = valA * valB;
    return;
-}*/
+}
 void computerArea(double valA, double valB, double& valArea, double& valPerimeter)
 {
    valArea = valA * valB;
    valPerimeter = (valA * 2) + (valB * 2);
+   return;
+}
+void  stats(double valA, double valB, double valC, double valD, double& valE, double& valF)
+{
+   valE = (valA + valB + valC + valD) / 4;
+   valF = valA + valB + valC + valD;
+   return;
+}*/
+void calcAreaPerimeter(double radius, double& area, double& perimeter)
+{
+   const double PI = 3.1415;
+   area = PI * (radius * radius);
+   perimeter = 2 * PI * radius;
    return;
 }
